ad774d438cffce4e5fcd585cfee5c34bed1a688b
Jake Hendley <jhendley25@Jakes-MacBook-Air.local>
2013-09-05 16:29:30 -0400

    first commit of hackathon app.  nearly done with UI

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+// 	$("#getting-started-btn").click(function(){
+// 		$(".getting-started").hide();
+// 		$(".getting-started-overlay").animate({
+// 			opacity: 0.25,
+// 			width: "0"},
+// 			1000, function(){
+
+// 			});
+
+// 		});
+// 		$(".img-holder").show();
+// 	})
